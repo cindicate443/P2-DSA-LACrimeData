7917c011781e78f591cdff21a627d211845f2ec0
jasage0 <jasage0@gmail.com>
2025-10-29 09:49:13 -0400

    added api

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,11 +7,18 @@ import './App.css'
 function App() {
   // Fetch backend/C++ output
   // For reference, region is the value, setRegion is the function to update the value, San Ferando is the defaul
-  const [region, setRegion] = useState("San Fernando Valley");
+  const [areas, setAreas] = useState([]);
+  const [lat, setLat] = useState([]);
+  const [lon, setLon] = useState([]);
+  const [data, setData] = useState([]);
   const [cppOutput, setCppOutput] = useState("");
 
+  const APP_TOKEN = "JZStIfxvIBLxyqzrOs41hWlyx" // api token from making an account with City of Los Angeles
+  const api = `https://data.lacity.org/api/v3/views/2nrs-mtv8/query.json/`; // api using SODA3
+
   // List of search types for buttons
-  const searchTypes = ["DFS", "BFS", "DISPLAY"];
+  //const searchTypes = ["DFS", "BFS", "DISPLAY"];
+  const [searchType, setSearchType] = useState(""); // using useState instead or array bc its easier to keep track when clicked (i.e onClick event)
 
   const search = (type) => {
     // Make HTTP request to backend
@@ -21,6 +29,36 @@ function App() {
       .catch(err => setCppOutput("Error: " + err));
   }
 
+  const fetchAreaNames = () => {
+    axios.get(api, { params: { query: "SELECT distinct area_name", app_token: APP_TOKEN } })
+    .then((response) => {
+        setAreas(response.data);
+        // console.log(response.data);
+    })
+  }
+
+  const fetchLAT = () => {
+    axios.get(api, { params: { query: "SELECT distinct lat", pageNumber: 1, pageSize: 10, app_token: APP_TOKEN } })
+    .then((response) => {
+        setLat(response.data);
+        //console.log(response.data);
+    })
+  }
+
+  const fetchLON = () => {
+    axios.get(api, { params: { query: "SELECT distinct lon", pageNumber: 1, pageSize: 10, app_token: APP_TOKEN } })
+    .then((response) => {
+        setLon(response.data);
+        //console.log(response.data);
+    })
+  }
+
+  useEffect(() => {
+    fetchAreaNames();
+    fetchLAT();
+    fetchLON();
+  }, [])
+
   return (
     <>
       <nav className='nav-cont'>
@@ -30,9 +68,10 @@ function App() {
       <div className='main'>
         <label> Select region
           <select>
-            <option value="San Fernando Valley">San Fernando Valley</option>
-            <option value="Antelope Valley.">Antelope Valley</option>
-            <option value="Central Los Angeles">Central Los Angeles</option>
+            {areas.map((item, key) => {
+              return (
+                <option key={key} value={item.area_name}>{item.area_name}</option>
+              )})}
           </select>
         </label>
 
@@ -42,23 +81,33 @@ function App() {
         </div>
 
         <div className='controls'>
-          <label> x-axis
-            <select></select>
-          </label>
+        <label> Longitude
+          <select>
+            {lon.map((item, key) => {
+              return (
+                <option key={key} value={item.lon}>{item.lon}</option>
+              )})}
+          </select>
+        </label>
 
-          <label> y-axis
-            <select></select>
-          </label>
+        <label> Latitude
+          <select>
+            {lat.map((item, key) => {
+              return (
+                <option key={key} value={item.lat}>{item.lat}</option>
+              )})}
+          </select>
+        </label>
 
         </div>
 
         <div className='search-buttons'>
-          <button>DFS Search</button>
-          <button>BFS Search</button>
+          <button onClick={() => setSearchType("DFS")}>DFS Search</button>
+          <button onClick={() => setSearchType("BFS")}>BFS Search</button>
         </div>
 
         <div>
-          <button>Display!</button>
+          <button onClick={() => setSearchType("Display")}>Display!</button>
         </div>
 
 
